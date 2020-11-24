a75a4abac1cf4d422b2496a7d20923fb07f86dee
sidneynkoba <sidneyknightmark.com>
2020-11-24 23:55:17 +0300

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
+
 
 class ProductController extends Controller
 {
+
+  public function index()
+  {
+    return ProductCollection::collection(Product::all());
+  }
+
+  public function create()
+  {
     //
+  }
+
+  public function store(Request $request)
+  {
+    //
+  }
+
+  public function show(Product $product)
+  {
+    return new ProductResource($product);
+  }
+
+  public function edit(Product $product)
+  {
+    //
+  }
+
+
+
 }
